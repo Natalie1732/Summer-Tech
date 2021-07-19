9b36ff667f773c5d49dce19636407005793a00ef
Natalie1732 <87618536+Natalie1732@users.noreply.github.com>
2021-07-19 10:03:15 -0400

    Update quiz game.py

--- quiz game.py
+++ quiz game.py
@@ -1,2 +1,8 @@
-
 print ("What is the purpose of flower petals?")
+word = input ("To attract bees (1)     To protect flower seeds (2)")
+if (input == 1) :
+    print ("correct, they are used to attract bees")
+elif (input == 2) :
+    print ("wrong, petals are used to attract bees")
+else :
+    print ("that wasn't an option")
