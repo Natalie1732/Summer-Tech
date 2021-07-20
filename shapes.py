56d03061af37b6e3b26d309771fcbb1796fb1e2a
Natalie1732 <87618536+Natalie1732@users.noreply.github.com>
2021-07-20 13:32:42 -0400

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -1,2 +1,14 @@
 for x in range (0,5) :
-    print ("*",end = "")
+    print ("")
+    for x in range (0,5) :
+        print ("*",end = "")
+
+for x in range (0,5) :
+    print ("")
+    for y in range (0,x) :
+        print ("*", end = "")
+
+for x in range (0,) :
+    print ("")
+    for y in range (0.) :
+        print ("*", end = "")
