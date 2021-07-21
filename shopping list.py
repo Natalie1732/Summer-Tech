43ff298d810427784e34e6e58e44a4c1c081706d
Natalie1732 <87618536+Natalie1732@users.noreply.github.com>
2021-07-21 13:31:09 -0400

    Update shopping list.py

--- shopping list.py
+++ shopping list.py
@@ -1,11 +1,13 @@
 print ("SHOPPING LIST")
+list = []
 bol = True
 while (bol) :
-    word1 = input ("Add something to the list.")
+    word1 = input ("Add something to the list:")
     word2 = input ("Are you done with the shopping list?")
-    list = [word1]
     if (word2 == " yes") :
         bol = False
-        print (list)
     elif (word2 == " no") :
         print ("What else do you need?")
+
+len (list)
+print (list)
