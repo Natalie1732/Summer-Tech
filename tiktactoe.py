4d60593e4c82c01f2891e23d58f1716486b2bfad
Natalie1732 <87618536+Natalie1732@users.noreply.github.com>
2021-07-22 11:10:46 -0400

    Update tiktactoe.py

--- tiktactoe.py
+++ tiktactoe.py
@@ -1,1 +1,9 @@
-
+list = [["*"]]
+for x in range (0,3):
+    print ("      *      *")
+for y in range (0,20):
+    print ("*", end = "")
+for z in range (0,3):
+    print ("      *      *")
+for a in range (0,20):
+    print ("*", end = "")
