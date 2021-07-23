2ccb771a8da72a1c927822b72412b9f8f004c644
Natalie1732 <87618536+Natalie1732@users.noreply.github.com>
2021-07-23 10:01:28 -0400

    Update tiktactoe.py

--- tiktactoe.py
+++ tiktactoe.py
@@ -1,4 +1,4 @@
-list = [[]]
+list = []
 for x in range (0,3):
     print ("")
     list.append ([])
@@ -6,4 +6,19 @@ for x in range (0,3):
         print ("*",end = "")
         list [x].append ("*")
 print (list, end = "")
-print (list [x] [y], end = "")
+
+for x in range (0, len (list)) :
+    print ("")
+    for y in range (0, len (list)) :
+        print (list [x] [y], end = "")
+
+word = input ("where would you like to go?")
+if (input == "1,1"):
+    list [0] [0] = "X"
+    print (list [x] [y], end = "")
+elif (input == "1,2"):
+    list [0] [1] = "x"
+    print (list [x] [y], end = "")
+if (input == "1,3"):
+    list [0] [2] = "x"
+    print (list [x] [y], end = "")
