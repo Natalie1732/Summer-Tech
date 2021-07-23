51a12eead6d48026816f50ef97e48513d5b4b4cd
Natalie1732 <87618536+Natalie1732@users.noreply.github.com>
2021-07-23 11:11:29 -0400

    Update tiktactoe.py

--- tiktactoe.py
+++ tiktactoe.py
@@ -12,13 +12,14 @@ for x in range (0, len (list)) :
     for y in range (0, len (list)) :
         print (list [x] [y], end = "")
 
-word = input ("where would you like to go?")
-if (input == "1,1"):
-    list [0] [0] = "X"
-    print (list [x] [y], end = "")
-elif (input == "1,2"):
-    list [0] [1] = "x"
-    print (list [x] [y], end = "")
-if (input == "1,3"):
-    list [0] [2] = "x"
-    print (list [x] [y], end = "")
+bol = True
+num = 0 + 1
+while (bol):
+    word1 = int (input ("which row would you like to go in? (0,1,2)"))
+    word2 = int (input ("which column would you like to go in? (0,1,2)"))
+    list [word1] [word2] = "X"
+    for x in range (0, len (list)) :
+        print ("")
+        for y in range (0, len (list)) :
+            print (list [x] [y], end = "")
+    if (num == 1)
